1963f89aaea5218ce492151ee3c4cad76fe3a456
divneev15 <divneevsingh123@gmail.com>
2025-08-05 22:27:25 +0530

    Create Concatination_in_strings.cpp

---
+++ Concatination_in_strings.cpp
@@ -0,0 +1,30 @@
+/* Divneev Singh
+ 24070123043
+ Entc A2
+ 
+*/
+
+#include <iostream>
+    using namespace std;
+    #include <string>
+    
+    int main(){
+     
+string a,b;
+    cout<<"Enter any word: \n";
+    cin>>a;
+    cout<<"Enter Another Word you want to combine: \n";
+    cin>>b;
+    cout<<a+" "+b<<"\n";
+    
+    return 0;
+    }
+
+/* OUTPUT
+
+Enter any word: 
+Divneev
+Enter Another Word you want to combine: 
+singh
+Divneev singh
+*/
