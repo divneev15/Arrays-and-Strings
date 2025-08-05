f604a2dc5883b876a5482f7f3f2670c0f82f21c4
divneev15 <divneevsingh123@gmail.com>
2025-08-05 22:24:33 +0530

    Create Entering_and_displaying_string.cpp

---
+++ Entering_and_displaying_string.cpp
@@ -0,0 +1,25 @@
+/* Divneev Singh
+ 24070123043
+ Entc A2
+ 
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str); 
+
+    cout << "You have entered: " << str << endl;
+
+    return 0;
+}
+
+/* output
+Enter a string: hello ! how are you ?
+You have entered: hello ! how are you ?
+*/
